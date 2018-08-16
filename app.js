3f0cf6db3682be82f32ad76929313338fce8d696
mobiisljh <40450537+mobiisljh@users.noreply.github.com>
2018-08-16 15:35:19 +0900

    Update app.js

--- app.js
+++ app.js
@@ -2,6 +2,6 @@ function onContentLoaded() {
   var accountSpan = document.getElementById("account");
 
   accountSpan.innerHTML =  
-    (typeof web3 === "undefined" ? "undefined" : web3.eth.accounts);
+    (typeof web3 === "undefined" ? "undefined" : web3.eth.accounts[0]);
 }
 document.addEventListener("DOMContentLoaded", onContentLoaded);
