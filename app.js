3aea752cf6a1940ea682696eeb2d41749045b153
mobiisljh <40450537+mobiisljh@users.noreply.github.com>
2018-08-16 15:24:16 +0900

    Update app.js

--- app.js
+++ app.js
@@ -1,8 +1,18 @@
 function onContentLoaded() {
   var accountSpan = document.getElementById("account");
+  var account;
+  var balance;
 
-  accountSpan.innerHTML =  
-    (typeof web3 === "undefined" ? "undefined" : web3.eth.accounts);
-  //web3.eth.accounts
-}
+  
+  account = web3.eth.accounts[0];
+  balance = web3.eth.getBalance(account);
+  
+  if(typeof web3 === "undefined"){
+    accountSpan.innerHTML =  
+    "undefined";
+  } else {
+    accountSpan.innerHTML =  
+    "account : " + account +"<br>"+ "balance : " + balance;
+  }
+  
 document.addEventListener("DOMContentLoaded", onContentLoaded);
